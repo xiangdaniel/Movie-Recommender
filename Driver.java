f476a72782008ab6a91ddf93458097f4fb33f1f7
Daniel D Xiang <32584509+xiangdaniel@users.noreply.github.com>
2018-01-30 09:49:45 -0800

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -7,6 +7,7 @@ public class Driver {
 		Normalize normalize = new Normalize();
 		Multiplication multiplication = new Multiplication();
 		Sum sum = new Sum();
+		RecommendModel recommendModel = new RecommendModel();
 
 		String rawInput = args[0];
 		String userMovieListOutputDir = args[1];
@@ -14,17 +15,21 @@ public class Driver {
 		String normalizeDir = args[3];
 		String multiplicationDir = args[4];
 		String sumDir = args[5];
+		String numberOfRecommendedMovies = args[6];
+
 		String[] path1 = {rawInput, userMovieListOutputDir};
 		String[] path2 = {userMovieListOutputDir, coOccurrenceMatrixDir};
 		String[] path3 = {coOccurrenceMatrixDir, normalizeDir};
 		String[] path4 = {normalizeDir, rawInput, multiplicationDir};
 		String[] path5 = {multiplicationDir, sumDir};
+		String[] path6 = {sumDir, rawInput, numberOfRecommendedMovies};
 		
 		dataDividerByUser.main(path1);
 		coOccurrenceMatrixGenerator.main(path2);
 		normalize.main(path3);
 		multiplication.main(path4);
 		sum.main(path5);
+		recommendModel.main(path6);
 
 	}
 
