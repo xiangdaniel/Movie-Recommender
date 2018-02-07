e109c4b427e35295bc952c53ba929cdd74019b7c
Daniel D Xiang <32584509+xiangdaniel@users.noreply.github.com>
2018-02-07 10:59:40 -0800

    Update DBOutputWritable.java

--- DBOutputWritable.java
+++ DBOutputWritable.java
@@ -8,19 +8,19 @@ public class DBOutputWritable implements DBWritable{
 
 	private int user_id;
 	private int movie_id;
-	private double rating;
+	private String rating;
 	
 	public DBOutputWritable(int user_id, int movie_id, double rating) {
 		this.user_id = user_id;
 		this.movie_id = movie_id;
-		this.rating = rating;
+		this.rating = Double.toString(rating);
 	}
 
 	public void readFields(ResultSet arg0) throws SQLException {
 
 		this.user_id = arg0.getInt(1);
 		this.movie_id = arg0.getInt(2);
-		this.rating = arg0.getDouble(3);
+		this.rating = arg0.getString(3);
 		
 	}
 
@@ -28,7 +28,7 @@ public class DBOutputWritable implements DBWritable{
 
 		arg0.setInt(1, user_id);
 		arg0.setInt(2, movie_id);
-		arg0.setDouble(3, rating);
+		arg0.setString(3, rating);
 		
 	}
 
